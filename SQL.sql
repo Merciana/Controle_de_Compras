7d0c67ea523139a9be58df0ed40102ec4258087d
Merciana <merciana.goncalves@gmail.com>
2025-01-29 08:40:35 -0300

    Atualizado

--- SQL.sql
+++ SQL.sql
@@ -7,31 +7,116 @@ DROP TABLE IF EXISTS compra;
 CREATE TABLE compra (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome_comprador VARCHAR(50),
-    ramal INT,
-    setor VARCHAR(50),
-    nome_produto VARCHAR(100),
+    nome_solicitante VARCHAR(50),
+    almoxerifado VARCHAR(50),
     nome_empresa VARCHAR(100),
-    nota_fiscal INT,
+    nota_fiscal BIGINT,
+    palavra_chave VARCHAR(50),
+    nome_produto VARCHAR(100),
     data_chegada DATE,
-    codigo_pedido INT,
     nome_porteiro VARCHAR(100),
     data_recebido DATE,
     tarefa_concluida TINYINT(1) DEFAULT 0,
     is_exemplo BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO compra (nome_comprador, ramal, setor, nome_produto, nome_empresa, nota_fiscal, data_chegada, codigo_pedido, nome_porteiro, data_recebido) VALUES
-('João Silva', 1234, 'Compras', 'Teclado', 'Empresa X', 123456, '2025-01-10', 101, 'Carlos Pereira', '2025-01-21'),
-('Maria Oliveira', 5678, 'TI', 'Monitor', 'Fornecedor Y', 789012, '2025-01-12', 102, 'Ana Souza', '2025-01-21'),
-('Carlos Souza', 2345, 'Logística', 'Mouse', 'Fornecedor Z', 345678, '2025-01-13', 103, 'Roberto Costa', '2025-01-21');
+INSERT INTO compra (nome_comprador, nome_solicitante, almoxerifado, nome_empresa, nota_fiscal, palavra_chave, nome_produto, data_chegada, nome_porteiro, data_recebido) VALUES
+('João Silva', 'Rafael silva', 'Marketing', 'Empresa X', 216516512, 'EH554FL', 'Teclado', '2025-01-10', 'Carlos Pereira', '2025-01-21'),
+('Maria Oliveira', 'Rafael silva', 'TI', 'Fornecedor Y', 5416816516, 'EH554FL', 'Impressora', '2025-01-12', 'Ana Souza', '2025-01-21'),
+('Carlos Souza', 'Rafael silva', 'Logística', 'Fornecedor Z', 65816168156, 'EH554FL', 'Maquina', '2025-01-13', 'Roberto Costa', '2025-01-21');
 
 UPDATE compra SET is_exemplo = TRUE WHERE id IN (1, 2, 3);
-
+ALTER TABLE compra AUTO_INCREMENT = 1;
 SELECT * FROM compra WHERE is_exemplo = FALSE;
-
-SHOW DATABASES;
-
-SHOW TABLES IN database_planilha;
 
 USE database_planilha;
 
+SHOW COLUMNS FROM compra;
+
+ALTER TABLE usuario_role DROP FOREIGN KEY usuario_role_ibfk_2;
+
+DROP TABLE IF EXISTS usuario_role;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS usuario;
+
+CREATE TABLE usuario (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+CREATE TABLE role (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+SELECT * FROM role WHERE name = '';
+
+UPDATE role SET name = 'default_role' WHERE name = '';
+
+INSERT INTO role (name) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+
+CREATE TABLE usuario_role (
+    usuario_id BIGINT,
+    role_id BIGINT,
+    PRIMARY KEY (usuario_id, role_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+
+INSERT INTO usuario (username, password) VALUES 
+('karla.lidiane', 'Tster021'),
+('diego.rogeiro', 'Mster021'),
+('yure.franca', 'Cster021'),
+('carlos.sigmund', 'TIster021'),
+('portaria', 'portaria'),
+('admin', 'admin!@#');
+
+
+INSERT INTO usuario_role (usuario_id, role_id)
+SELECT u.id, r.id
+FROM usuario u
+JOIN role r ON r.name = 'ROLE_USER'
+WHERE u.username = 'portaria';
+
+-- "admin" com a role ROLE_ADMIN
+INSERT INTO usuario_role (usuario_id, role_id)
+SELECT u.id, r.id
+FROM usuario u
+JOIN role r ON r.name = 'ROLE_ADMIN'
+WHERE u.username = 'karla.lidiane';
+
+INSERT INTO usuario_role (usuario_id, role_id)
+SELECT u.id, r.id
+FROM usuario u
+JOIN role r ON r.name = 'ROLE_ADMIN'
+WHERE u.username = 'diego.rogeiro';
+
+INSERT INTO usuario_role (usuario_id, role_id)
+SELECT u.id, r.id
+FROM usuario u
+JOIN role r ON r.name = 'ROLE_ADMIN'
+WHERE u.username = 'yure.franca';
+
+INSERT INTO usuario_role (usuario_id, role_id)
+SELECT u.id, r.id
+FROM usuario u
+JOIN role r ON r.name= 'ROLE_ADMIN'
+WHERE u.username = 'carlos.sigmund';
+
+INSERT INTO usuario_role (usuario_id, role_id)
+SELECT u.id, r.id
+FROM usuario u
+JOIN role r ON r.name = 'ROLE_ADMIN'
+WHERE u.username = 'admin';
+
+SELECT u.username, r.name AS role
+FROM usuario u
+JOIN usuario_role ur ON ur.usuario_id = u.id
+JOIN role r ON ur.role_id = r.id;
+
+SHOW CREATE TABLE usuario_role;
+
+SELECT username, password FROM usuario;
+
+SHOW COLUMNS FROM role;
